8217315b9c6f79dc230431000738346f316f367d
Iremide-ds <iremideadeboye@gmail.com>
2025-11-09 06:50:35 +0100

    supabase initialized for project

--- lib/src/shared/util/init.dart
+++ lib/src/shared/util/init.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:teams_gen/firebase_options.dart';
+import 'package:teams_gen/src/core/config/env.dart';
 import 'package:teams_gen/src/core/service/data_cache.dart';
 
 initServices() {}
@@ -7,6 +9,13 @@ initServices() {}
 Future<FirebaseApp> initFirebaseCore() {
   return Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+}
+
+Future<Supabase> initSupabase() {
+  return Supabase.initialize(
+    url: Env.supabaseURL,
+    anonKey: Env.supabaseAnonKey,
   );
 }
 
